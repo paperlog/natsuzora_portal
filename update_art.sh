3739cdc384c0357ba74476981467e2f1d8f6e4f0
paperlog <yuu625581@gmail.com>
2026-09-23 19:01:45 +0900

    データ編集用コードの整理

--- update_art.sh
+++ update_art.sh
@@ -4,6 +4,4 @@ cd ~/program/HTML_CSS/natsuzora_portal
 source .venv/bin/activate 
 python art_add.py
 deactivate
-git add .
-git commit -m "ファンアートの追加・更新"
-git push
+echo "ファンアートの追加・更新"
--- update_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/program/HTML_CSS/natsuzora_portal
-python videos_add.py
-git add .
-git commit -m "動画の追加・更新"
-git push
